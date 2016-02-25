44bd8e4af65ddc66808c57be40e841b77b7d8b5f
Michael Jeffrey Wu <michaeljeffreywu@gmail.com>
2016-02-24 17:29:20 -0800

    [feature] connected to sheetsu API

--- controllers/form-controller.js
+++ controllers/form-controller.js
@@ -1,18 +1,40 @@
 angular.module('formApp')
-.controller('formController', function($scope) {
+.controller('formController', function($scope, $http) {
     $scope.cookies = ['chocolate_chip', 'macadamian_nut', 'white_chocolate', 'oatmeal_raisin'];
 
     // we will store all of our form data in this object
     $scope.formData = {
-        'oatmeal_raisin': 0,
-        'macadamian_nut': 0,
-        'white_chocolate': 0,
-        'chocolate_chip': 0
+        cookie: {
+            'oatmeal_raisin': 0,
+            'macadamian_nut': 0,
+            'white_chocolate': 0,
+            'chocolate_chip': 0
+        }
     };
     
     // function to process the form
     $scope.processForm = function() {
-        alert('awesome!');  
+        // for (var cookie in $scope.formData.cookie) {
+        //     if ($scope.formData.cookie[cookie] == 0) {
+        //         delete cookie;
+        //     }
+        // }
+        $scope.formData.Cookie = '';
+        angular.forEach($scope.formData.cookie, function(value, key) {
+            if (value == 0) {
+                delete $scope.formData.cookie[key];
+            }
+            else {
+                $scope.formData.Cookie += key + ' ' + value + ' ';
+            }
+        });
+        delete $scope.formData.cookie;
+
+         $http.post('https://sheetsu.com/apis/2ae6fdf1', $scope.formData)
+         .success(function(data, status) {
+            if data.success
+                alert('')
+         })
     };
     
 });
